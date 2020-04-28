abee059d4049c2066d4bdef47ebb340d433cd073
Firat <fratpalta@gmail.com>
2020-04-29 01:53:25 +0300

    hizmetlerimiz arka logo silindi

--- js/index.js
+++ js/index.js
@@ -11,42 +11,44 @@ async function slide() {
   let x = document.getElementsByClassName("mySlides");
   var arr = [...x];
   const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-  let el;
+  console.log(arr)
 
   while (true) {
-    for(let i=0; i<16; i+=4) {
+    for(let i=0; i<24; i+=6) {
       arr[i].style.transform = "translateY(0)";
       arr[i+1].style.transform = "translateY(100%)";
       arr[i+2].style.transform = "translateY(-100%)";
       arr[i+3].style.transform = "translateY(100%)";
+      arr[i+4].style.transform = "translateY(-100%)";
+      arr[i+5].style.transform = "translateY(100%)";
     }
     await delay(2500);
-    for(let i=0; i<16; i+=4) {
+    for(let i=0; i<24; i+=6) {
       arr[i].style.transform = "translateY(-100%)";
       arr[i+1].style.transform = "translateY(0)";
     }   
     await delay(2500);
-    for(let i=0; i<16; i+=4) {
+    for(let i=0; i<24; i+=6) {
       arr[i+1].style.transform = "translateY(100%)";
       arr[i+2].style.transform = "translateY(0)";
     }       
     await delay(2500);
-    for(let i=0; i<16; i+=4) {
+    for(let i=0; i<24; i+=6) {
       arr[i+2].style.transform = "translateY(-100%)";
       arr[i+3].style.transform = "translateY(0)";
     }   
     
     await delay(2500);
-    for(let i=0; i<16; i+=4) {
+    for(let i=0; i<24; i+=6) {
       arr[i+3].style.transform = "translateY(100%)";
-      arr[i].style.transform = "translateY(0)";
-    }   
+      arr[i+4].style.transform = "translateY(0)";
+    } 
     
- 
-    // for(let i= 0; i<4 ; i++) {
-    //   el = arr.shift()
-    //   arr.push(el);
-    // } 
-    
+    await delay(2500);
+    for(let i=0; i<24; i+=6) {
+      arr[i+4].style.transform = "translateY(-100%)";
+      arr[i+5].style.transform = "translateY(0)";     
+    }
+    await delay(2500);
   }
 }
